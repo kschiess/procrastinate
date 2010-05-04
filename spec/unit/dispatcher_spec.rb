6b56d1e90628fb283306b51bb10af7be8f0d8dd3
Kaspar Schiess <kaspar.schiess@geo.uzh.ch>
2010-05-04 13:52:14 +0200

    . fixes dispatcher tests

--- spec/unit/dispatcher_spec.rb
+++ spec/unit/dispatcher_spec.rb
@@ -14,7 +14,7 @@ describe Procrastinate::Dispatcher do
       # Worker class expectation
       worker_klass = Class.new
       worker_klass.instance_eval do
-        define_method(:message) { |a,b,c| puts "holler from #{Process.pid}" }
+        define_method(:message) { |a,b,c|  }
       end
       
       @dispatcher = Procrastinate::Dispatcher.start(strategy, worker_klass)
@@ -27,13 +27,18 @@ describe Procrastinate::Dispatcher do
     end 
     context "that expects completion to be called" do
       before(:each) do
-        button = flexmock().should_receive(:push).once.mock
+        @spawn_called = false
+        
+        button = flexmock().should_receive(:push).at_least.once.mock
         strategy.should_receive(:spawn_new_workers).and_return do |spawner|
           spawner.spawn([:message, [1,2,3], nil]) { button.push }
+          @spawn_called = true
         end
       end
 
       it "should start the task and call its callback" do
+        dispatcher.wakeup
+        sleep 0.01 until @spawn_called
         dispatcher.wakeup
         dispatcher.stop
       end
