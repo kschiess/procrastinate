13e6c200069b2d295e905f8d141ed175301494ae
Kaspar Schiess <kaspar.schiess@absurd.li>
2010-05-05 11:15:28 +0200

    + Removes a lot of duplicate code
    Strategies will always be some variation on the simple strategy.

--- lib/procrastinate/dispatch_strategy/simple.rb
+++ lib/procrastinate/dispatch_strategy/simple.rb
@@ -3,7 +3,8 @@ require 'thread'
 
 class Procrastinate::DispatchStrategy::Simple
   attr_reader :queue
-  
+
+  # Client thread
   def initialize
     @queue = Queue.new
     @shutdown_requested = false
@@ -13,18 +14,31 @@ class Procrastinate::DispatchStrategy::Simple
     @shutdown_requested
   end
 
+  # Client thread
   def schedule(task)
     raise ShutdownRequested if shutdown_requested?
     
     queue.push task
   end
   
+  # Dispatcher thread
   def spawn_new_workers(dispatcher)
     # Spawn tasks
-    dispatcher.spawn queue.pop until queue.empty?
+    spawn(dispatcher) while should_spawn?
 
     # If the queue is empty now, maybe shutdown the dispatcher
     dispatcher.request_stop if shutdown_requested? && queue.empty?
+  end
+  
+  
+  # Spawn a new task from the job queue. 
+  # Dispatcher thread
+  #
+  def spawn(dispatcher, &block)
+    dispatcher.spawn(queue.pop, &block)
+  end
+  def should_spawn?
+    not queue.empty?
   end
   
   def shutdown
--- lib/procrastinate/dispatch_strategy/throttled.rb
+++ lib/procrastinate/dispatch_strategy/throttled.rb
@@ -2,53 +2,24 @@
 # A dispatcher strategy that throttles tasks starting and ensures that no
 # more than limit processes run concurrently. 
 #
-class Procrastinate::DispatchStrategy::Throttled
-  attr_reader :queue
+class Procrastinate::DispatchStrategy::Throttled < Procrastinate::DispatchStrategy::Simple
   attr_reader :limit, :current
   
   # Client thread
   def initialize(limit)
-    @queue = Queue.new
-    @shutdown_requested = false
+    super()
     
     @limit = limit
     @current = 0
   end
-
-  # All threads
-  def shutdown_requested?
-    @shutdown_requested
-  end
-
-  # Client thread
-  def schedule(task)
-    raise ShutdownRequested if shutdown_requested?
-    
-    queue.push task
-  end
-  
-  # Dispatcher thread
-  def spawn_new_workers(dispatcher)
-    # Spawn tasks
-    spawn(dispatcher) while should_spawn?
-
-    # If the queue is empty now, maybe shutdown the dispatcher
-    dispatcher.request_stop if shutdown_requested? && queue.empty?
-  end
   
   # Dispatcher thread
   def spawn(dispatcher)
-    dispatcher.spawn(queue.pop) { @current -= 1 }
+    super(dispatcher) { @current -= 1 }
     @current += 1
   end
-  # Dispatcher thread
   def should_spawn?
-    (not queue.empty?) &&
+    super &&
       current < limit
-  end
-  
-  # Client thread
-  def shutdown
-    @shutdown_requested = true
   end
 end
