ff5bee26988242be230be45bbf1665b7b070fe86
Kaspar Schiess <kaspar.schiess@absurd.li>
2010-05-05 08:57:56 +0200

    . a simple example

---
+++ examples/simple.rb
@@ -0,0 +1,21 @@
+
+$:.unshift File.dirname(__FILE__) + '/../lib'
+
+require 'procrastinate'
+
+class Worker
+  def do_work
+    puts "> Starting work in process #{Process.pid}"
+    sleep 10
+    puts "< Work completed in process #{Process.pid}"
+  end
+end
+
+scheduler = Procrastinate::Scheduler.start
+worker = scheduler.create_proxy(Worker)
+
+10.times do 
+  worker.do_work
+end
+
+scheduler.shutdown
