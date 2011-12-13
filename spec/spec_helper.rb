65f78ca765b278d70f32c12ee5873f5391c168ac
Kaspar Schiess <kaspar.schiess@geo.uzh.ch>
2011-12-13 16:08:59 +0100

    .

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,7 @@ require 'tempfile'
 require 'procrastinate'
 
 trap('INFO') {  
+  puts "You sent me a SIGINFO at #{Time.now}."
   Thread.list.each do |thread|
     p thread
     p thread.backtrace.first(5)
