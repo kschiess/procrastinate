ca2f63a6eccab9726b195c211a3f478145bbcaca
Kaspar Schiess <kschiess@gironimo.geo.uzh.ch>
2011-12-16 14:29:09 +0100

    ! no siginfo on linux

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,11 +8,15 @@ require 'tempfile'
 
 require 'procrastinate'
 
-trap('INFO') {  
-  puts "You sent me a SIGINFO at #{Time.now}."
-  Thread.list.each do |thread|
-    p thread
-    p thread.backtrace.first(5)
-    puts
-  end
-}
+begin
+  trap('INFO') {  
+    puts "You sent me a SIGINFO at #{Time.now}."
+    Thread.list.each do |thread|
+      p thread
+      p thread.backtrace.first(5)
+      puts
+    end
+  }
+rescue ArgumentError
+  # No SIGINFO on some systems.
+end
